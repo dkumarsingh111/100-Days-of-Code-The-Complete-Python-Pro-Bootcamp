f1fae5e8e675b4c14fd4ef666554c286b56166b7
Deepak Singh <Deepak.Singh@bd.com>
2024-05-07 13:54:08 +0530

    add File with command

--- Day-8/parameterized_function.py
+++ Day-8/parameterized_function.py
@@ -3,4 +3,5 @@ def greet_with(name, location):
     print(f"What is it like in {location}")
 
 
-greet_with("Deepak", "Noida")    
+greet_with(location="Noida", name="Deepak")   
+greet_with("Deepak","Noida")   
