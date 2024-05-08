01b29daf5943f8b1949790c7c49e31040ff3d423
Deepak Singh <153359201+dkumarsingh111@users.noreply.github.com>
2024-05-08 17:53:13 +0530

    Add files via upload

--- Day-9/grading.py
+++ Day-9/grading.py
@@ -18,3 +18,15 @@ for name in student_scores:
 
 
 print(student_scores)
+
+
+
+
+
+order = {
+    "starter": {1: "Salad", 2: "Soup"},
+    "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+    "dessert": {1: ["Ice Cream"], 2: []},
+}
+
+print(order["main"][2][0])
