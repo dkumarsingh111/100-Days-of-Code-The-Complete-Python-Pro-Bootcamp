b60f6b29c4669fd898ccab539b5ebc85183e9bc6
Deepak Singh <Deepak.Singh@bd.com>
2024-05-10 14:26:59 +0530

    add files via command prompt

--- Day-9/auction.py
+++ Day-9/auction.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 print("Welcome to the secret auction program!")
@@ -22,29 +21,4 @@ for name in auction_record:
         max_auction_amount = int(auction_record[name])
         winner_name = name
 
-=======
-import os
-
-print("Welcome to the secret auction program!")
-
-is_continue = "yes"
-auction_record = {}
-max_auction_amount = 0
-winner_name = ''
-
-while is_continue == "yes":
-    name = input("What is your name?: ")
-    bid = int(input("What's your bid?: $"))
-
-    auction_record[name] = bid
-
-    is_continue = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
-    os.system('cls||clear')
-    
-for name in auction_record:
-    if int(auction_record[name]) > max_auction_amount:
-        max_auction_amount = int(auction_record[name])
-        winner_name = name
-
->>>>>>> c7487286b219193de3e367d4bcd2d00572d4ddf7
 print(f"The winner is {winner_name} with a bid of ${max_auction_amount}")
