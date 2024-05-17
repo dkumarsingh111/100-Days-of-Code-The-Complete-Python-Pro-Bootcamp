5fbb6f03eca93f8a448125893d51e1315d20806c
Deepak Singh <Deepak.Singh@bd.com>
2024-05-17 14:45:14 +0530

    add file via WS

---
+++ Day-16/Machine.py
@@ -0,0 +1,8 @@
+class Machine:
+    dict_coffee = {
+                    'Cappuccino' : 100,
+                    'Cafe Latte' : 200,
+                    'Espresso' : 300
+                }
+    quantity = [2,2,2]
+    power_status = False
